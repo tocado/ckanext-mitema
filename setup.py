4c7fe8d90d56858dc7be94b36d15c577d857abb0
tocado <tocado.157@gmail.com>
2025-06-30 20:50:04 -0300

    actualizacion setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mitema',
+    name='ckanext-mitema',
     version='0.0.1',
     description='Tema personalizado para CKAN',
     author='Tu Nombre',
     license='MIT',
-    packages=find_packages('ckanext'),  # ?? importante
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.mitema'],
     package_dir={'': 'ckanext'},        # ?? importante
     include_package_data=True,
     zip_safe=False,
