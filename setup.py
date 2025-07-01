753a834a1a99ea3e102855660595f8be296245e7
Juan Sebastian Romero <tocado.157@gmail.com>
2025-07-01 17:51:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packagesAdd commentMore actions
 
 setup(
-    name='mitema',
+    name='ckanext-mitema',
     version='0.0.1',
     description='Tema personalizado para CKAN',
-    author='Tu Nombre',
+    author='Adrian Figueroa',
     license='MIT',
-    packages=find_packages('ckanext'),  # ?? importante
+    packages=find_packages(),  # ?? importante
+
     include_package_data=True,
     zip_safe=False,
     entry_points='''
         [ckan.plugins]
         mitema=ckanext.mitema.plugin:MiTemaPlugin
-        
+
         [ckan.templates]
         mitema=ckanext.mitema
     ''',
-)
