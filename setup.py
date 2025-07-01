e1533421d5c65d9548732019cf09916d6a6cc597
tocado <tocado.157@gmail.com>
2025-06-30 22:01:05 -0300

    actualizo entry point

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ckanext-mitema',
+    name='mitema',
     version='0.0.1',
     description='Tema personalizado para CKAN',
     author='Tu Nombre',
     license='MIT',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    package_dir={'': 'ckanext'},        # ?? importante
+    packages=find_packages('ckanext'),  # ?? importante
     include_package_data=True,
     zip_safe=False,
     entry_points='''
         [ckan.plugins]
         mitema=ckanext.mitema.plugin:MiTemaPlugin
-
+        
         [ckan.templates]
         mitema=ckanext.mitema
     ''',
