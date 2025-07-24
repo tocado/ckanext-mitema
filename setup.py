37ca8727d8ae851c6fba5caff7ddf4e1f66af341
tocado <tocado.157@gmail.com>
2025-07-24 19:19:40 -0300

    vuelvo a setup que funcionaba

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mitema',
+    name='ckanext-mitema',
     version='0.0.1',
     description='Tema personalizado para CKAN',
-    author='Tu Nombre',
+    author='Adrian Figueroa',
     license='MIT',
-    packages=find_packages("ckanext"),
-    package_dir={'': '.'},
+    packages=find_packages(),  # ?? importante
     include_package_data=True,
     zip_safe=False,
     entry_points='''
         [ckan.plugins]
         mitema=ckanext.mitema.plugin:MiTemaPlugin
-        
+
         [ckan.templates]
         mitema=ckanext.mitema
     ''',
